56fdd6a151440984393d72be944f8a253dcd0313
Lucas Cordova <lpcordova@outlook.com>
2025-09-10 08:51:30 -0700

    Moving car for workshop 2 day 2

--- Assets/Cruise.cs
+++ Assets/Cruise.cs
@@ -18,37 +18,30 @@ public class Cruise : MonoBehaviour
         float move = 0f;
         float steer = 0f;
 
-        Debug.Log("Crusing...");
-
         if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
         {
-            Debug.Log("Forward movement");
             move = 1f;
         }
+
         else if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
         {
-            Debug.Log("Backward movement");
-            move = -1;
+            move = -1f;
         }
-        else if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
-        {
-            Debug.Log("Left movement");
-            steer = -1f;
 
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            steer = 1f;
         }
+
         else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
         {
-            Debug.Log("Right movement");
-
             steer = -1f;
         }
-
-        float moveAmount = move * Time.deltaTime * currentSpeed;
-        float steerAmount = steer * Time.deltaTime * steerSpeed;
+        
+        float moveAmount = move * currentSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
 
         transform.Translate(0, moveAmount, 0);
         transform.Rotate(0, 0, steerAmount);
-        
-        
     }
 }
